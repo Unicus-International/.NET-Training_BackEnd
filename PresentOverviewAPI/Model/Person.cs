6635f18e7823014f41bbda34b64cb7b343836334
Knut Åge Hofseth <Knut-hof@online.no>
2024-06-11 15:24:14 +0200

    Implement simple InMemoryDb

--- PresentOverviewAPI/Model/Person.cs
+++ PresentOverviewAPI/Model/Person.cs
@@ -3,5 +3,7 @@
     public class Person
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
---
+++ PresentOverviewAPI/Model/dbContext/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PresentOverviewAPI.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Person> Persons { get; set; } = null!;
+    }
+}
