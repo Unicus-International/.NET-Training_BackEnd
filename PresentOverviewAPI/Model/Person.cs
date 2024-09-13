276a5538409b324ac5ce8b455a8f3b4c591e7685
Knut-Aage-Hofseth <Knut-hof@online.no>
2024-09-13 12:36:15 +0200

    More Experimentation

--- PresentOverviewAPI/Model/Person.cs
+++ PresentOverviewAPI/Model/Person.cs
@@ -8,6 +8,7 @@ namespace PresentOverviewAPI.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
     }
 }
